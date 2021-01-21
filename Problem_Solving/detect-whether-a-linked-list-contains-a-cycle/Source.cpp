8c1c63752ea945e06936be4c5bb61ec3e7a0df02
Sherif .M Samy <eng.SherifMostafa25@gmail.com>
2021-01-22 00:19:57 +0200

    solve problem

--- Problem_Solving/detect-whether-a-linked-list-contains-a-cycle/Source.cpp
+++ Problem_Solving/detect-whether-a-linked-list-contains-a-cycle/Source.cpp
@@ -10,6 +10,20 @@
  *
  */
 bool has_cycle(SinglyLinkedListNode* head) {
+    if (head == nullptr)
+        return 0;
     
+    auto slow = head;
+    auto fast = head;
 
+    while (fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast){
+            return 1;
+        }
+    }
+    
+    return 0;
 }
