9c60e25b4d4ef0ba0cd318bbe839c3350bf68fe9
Sherif .M Samy <eng.SherifMostafa25@gmail.com>
2021-01-22 01:52:43 +0200

    solve problem

--- Problem_Solving/find-the-merge-point-of-two-joined-linked-lists/Source.cpp
+++ Problem_Solving/find-the-merge-point-of-two-joined-linked-lists/Source.cpp
@@ -10,6 +10,25 @@
  *
  */
 int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    auto cursor1 = head1;
+    auto cursor2 = head2;
 
-
+    while (cursor1 != cursor2) {
+        //cursor1
+        if(cursor1->next == null)
+            cursor1 = head2;
+			
+		else
+            cursor1 = cursor1->next;
+		
+        //cursor2
+        if(cursor2->next == null)
+            cursor2 = head1;
+        
+		else
+            cursor2 = cursor2->next;
+			
+    }
+	
+    return cursor1->data;
 }
