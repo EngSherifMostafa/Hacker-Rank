6d02b5788a1f1cbda5f75b73121af87a534dbcea
Sherif .M Samy <eng.SherifMostafa25@gmail.com>
2021-01-23 12:56:59 +0200

    initial template

---
+++ Problem_Solving/array-and-simple-queries/Source.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
